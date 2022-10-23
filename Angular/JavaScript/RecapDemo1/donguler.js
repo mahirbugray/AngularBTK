e4c6a13f4dcf26bef47f55145cee331eef3dc2a1
mahirbugray <74426348+mahirbugray@users.noreply.github.com>
2022-10-23 20:03:24 +0300

    New commit

---
+++ Angular/JavaScript/RecapDemo1/donguler.js
@@ -0,0 +1,26 @@
+// for(i=1;i<=10;i++){
+//     console.log(i);
+// }
+// console.log("Bitti");
+
+var sehirler = ["Ankara", "Istanbul", "Izmir", "Adana"];
+
+// for(i=0;i<sehirler.length;i++){
+//     console.log(sehirler[i]);
+// }
+
+// var i=0;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+// var i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 10);
+
+sehirler.forEach(function (sehir) {
+  console.log(sehir);
+});
---
+++ Angular/JavaScript/RecapDemo1/index.js
@@ -0,0 +1,27 @@
+var kullanicilar = [
+    { email: "engindemirog@gmail.com", sifre: "12345" },
+    { email: "derindemirog@gmail.com", sifre: "12345" },
+  ];
+  
+  var twitler = [
+    { email: "engindemirog@gmail.com", twit: "Bugün hava çok güzel." },
+    { email: "engindemirog@gmail.com", twit: "Bugün hava çok güzel 2." },
+    { email: "derindemirog@gmail.com", twit: "Kapıyı ben açacağım." },
+  ];
+  
+  var email = prompt("Email?");
+  var sifre = prompt("Sifre?");
+  
+  function giris() {
+    if (
+      (email == kullanicilar[0].email && sifre == kullanicilar[0].sifre) ||
+      (email == kullanicilar[1].email && sifre == kullanicilar[1].sifre)
+    ) {
+      console.log(twitler);
+    }
+    else{
+      console.log("Kullanici adi veya sifre hatali");
+    }
+  }
+  
+  giris(email,sifre)

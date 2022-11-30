309e58b1eed8f604dcc29427ada779fb95f62ae2
mahirbugray <74426348+mahirbugray@users.noreply.github.com>
2022-11-30 19:18:28 +0300

    last commit

--- Angular/TypeScript/function.ts
+++ Angular/TypeScript/function.ts
@@ -14,3 +14,24 @@ console.log(topla(2, 3));
 console.log(topla("Ankara", 3));
 console.log(topla2(2, 4));
 console.log(topla3(4, 8));
+
+function topla4(x: number, y: number = 4): number {
+  return x + y;
+}
+
+console.log(topla4(3));
+
+function topla5(x: number, y?: number): number {
+  if (y) {
+    return x + y;
+  }
+  return x;
+}
+
+console.log(topla5(3, 40));
+
+function davetEt(ilkDavetli: string, ...digerleri: string[]): string {
+  return ilkDavetli + " " + digerleri.join(" ");
+}
+
+console.log(davetEt("Mahir", "Murat", "Tunahan"));

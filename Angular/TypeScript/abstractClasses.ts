c077d336c49a53871b4ed94a6312eff578fa78c3
mahirbugray <74426348+mahirbugray@users.noreply.github.com>
2022-12-01 20:37:35 +0300

    abstract

---
+++ Angular/TypeScript/abstractClasses.ts
@@ -0,0 +1,43 @@
+abstract class KrediBase {
+  constructor() {}
+  kaydet(): void {
+    console.log("Kaydedildi");
+  }
+
+  abstract hesapla(): void;
+}
+
+class TuketiciKredi extends KrediBase {
+  constructor() {
+    super();
+  }
+
+  hesapla(): void {
+    console.log("Tüketici kredisine göre hesap yapıldı.");
+  }
+}
+
+class MortgageKredi extends KrediBase {
+  constructor() {
+    super();
+  }
+
+  hesapla(): void {
+    console.log("Konut kredisine göre hesap yapıldı.");
+  }
+
+  baskaBirOperasyon() {}
+}
+
+let tuketiciKredisi = new TuketiciKredi();
+tuketiciKredisi.hesapla();
+tuketiciKredisi.kaydet();
+
+let mortgageKredisi = new MortgageKredi();
+mortgageKredisi.hesapla();
+mortgageKredisi.kaydet();
+
+let kredi: KrediBase;
+kredi = new TuketiciKredi();
+
+kredi = new MortgageKredi();
